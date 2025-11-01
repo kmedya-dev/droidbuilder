1e25d81dbd9788628e705969ad1f09342628ea51
kmedya <koustavamedya.2000@gmail.com>
2025-11-01 06:15:07 +0530

    update

--- droidbuilder/utils/package_resolver.py
+++ droidbuilder/utils/package_resolver.py
@@ -258,7 +258,7 @@ def _resolve_from_pypi(name, version=None):
         logger.error(f"An unexpected error occurred while processing {name}: {e}")
         return None, None
 
-def resolved_packages(conf):
+def resolve_packages(conf):
     """
     Resolves packages.
     It first checks the dependency mapping. If not mapped, try with PyPI. If not found, it falls back to search online.
