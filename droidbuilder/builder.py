576371f96396b79c87f8197dc540effb734397c6
kmedya <koustavamedya.2000@gmail.com>
2025-11-28 16:07:20 +0530

    update

--- droidbuilder/builder.py
+++ droidbuilder/builder.py
@@ -75,7 +75,7 @@ def _build_python_for_android(python_version, package_config, python_host, pytho
         "--enable-optimizations",
         "--with-lto",
         f"--with-build-python={python_host}",
-        f"--with-openssl={env_obj.pkg_config_executable}",
+        f"--with-openssl={env_obj.pkg_config_path}",
     ]
 
     commands = resolve_config_type(
@@ -100,7 +100,7 @@ def _build_python_for_android(python_version, package_config, python_host, pytho
         strip=env_obj.strip_path,
         ndk_root=env_obj.ndk_root,
         sysroot=env_obj.sysroot,
-        pkg_config=env_obj.pkg_config_executable,
+        pkg_config=env_obj.pkg_config_path,
         extra_configure_args=python_configure_args,
     )
 
@@ -176,7 +176,7 @@ def _compile_runtime_package(package_name, package_config, runtime_package_sourc
     pip_env = env_obj.env.copy() # Use the env returned by _setup_build_environment
     pip_env["CFLAGS"] = env_obj.cflags
     pip_env["LDFLAGS"] = env_obj.ldflags
-    pip_env["PKG_CONFIG"] = env_obj.pkg_config_executable
+    pip_env["PKG_CONFIG"] = env_obj.pkg_config_path
 
     pip_install_cmd = [
         python_bin, # Path to target Python interpreter
@@ -236,7 +236,7 @@ def _compile_buildtime_package(package_name, package_config, buildtime_package_s
         strip=env_obj.strip_path,
         ndk_root=env_obj.ndk_root,
         sysroot=env_obj.sysroot,
-        pkg_config=env_obj.pkg_config_executable,
+        pkg_config=env_obj.pkg_config_path,
         extra_configure_args=extra_configure_args,
     )
 
--- droidbuilder/utils/toolchain_manager.py
+++ droidbuilder/utils/toolchain_manager.py
@@ -34,8 +34,7 @@ class BuildEnvironment:
         self.ndk_root = None
         self.compiler_prefix = None
         self.env = None
-        self.pkg_config_libdir = None
-        self.pkg_config_executable = None
+        self.pkg_config_path = None
         self.setup()
 
     def setup(self):
@@ -79,12 +78,9 @@ class BuildEnvironment:
         self.asmflags = f"-fPIC -DANDROID"
         self.ldflags = "-lm -ldl"
 
-        self.pkg_config_libdir = os.path.join(self.build_path, "buildtime-install", self.arch, "lib", "pkgconfig")
-        self.pkg_config_executable = shutil.which("pkg-config")
-
-        if not self.pkg_config_executable:
-            logger.warning("  - 'pkg-config' executable not found in PATH. Some packages may fail to build.")
-            self.pkg_config_executable = "pkg-config"
+        self.pkg_config_path = os.path.join(self.build_path, "buildtime-install", self.arch, "lib", "pkgconfig")
+        if not self.pkg_config_path:
+		self.pkg_config_path = shutil.which("pkg-config")
 
         # Prepare environment variables for subprocesses
         self.env = os.environ.copy()
@@ -94,7 +90,7 @@ class BuildEnvironment:
         self.env["CXXFLAGS"] = f"{os.environ.get('CXXFLAGS', '')} {self.cxxflags}".strip()
         self.env["ASMFLAGS"] = f"{os.environ.get('ASMFLAGS', '')} {self.asmflags}".strip()
         self.env["LDFLAGS"] = f"{os.environ.get('LDFLAGS', '')} {self.ldflags}".strip()
-        self.env["PKG_CONFIG_PATH"] = f"{os.environ.get('PKG_CONFIG_PATH', '')} {self.pkg_config_libdir}".strip()
+        self.env["PKG_CONFIG_PATH"] = f"{os.environ.get('PKG_CONFIG_PATH', '')} {self.pkg_config_path}".strip()
 
         self.env["AR"] = self.ar_path
         self.env["AS"] = self.as_path
